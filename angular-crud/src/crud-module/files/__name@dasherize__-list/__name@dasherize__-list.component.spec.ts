cc586d8c2a8f27cfe0e28057ae5243bd0387fa7a
Matt Raible <matt.raible@okta.com>
2019-11-22 16:28:05 -0700

    Fix import

--- angular-crud/src/crud-module/files/__name@dasherize__-list/__name@dasherize__-list.component.spec.ts
+++ angular-crud/src/crud-module/files/__name@dasherize__-list/__name@dasherize__-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { <%=classify(name)%>ListComponent } from './<%=dasherize(name)%>-List.component';
+import { <%=classify(name)%>ListComponent } from './<%=dasherize(name)%>-list.component';
 import { <%=classify(name)%>Service } from '../<%=dasherize(name)%>.service';
 
 describe('<%=classify(name)%>ListComponent', () => {
