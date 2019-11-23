0015c8087f2ffdc85188a1113c61c3a49567e65d
Matt Raible <matt.raible@okta.com>
2019-11-23 11:08:52 -0700

    Formatting

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { FlightModule } from './flight/flight.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {APP_EXTRA_OPTIONS, APP_ROUTES} from './app.routes';
-import {HomeComponent} from './home/home.component';
-import {NavbarComponent} from './navbar/navbar.component';
-import {SidebarComponent} from './sidebar/sidebar.component';
+import { AppComponent } from './app.component';
+import { APP_EXTRA_OPTIONS, APP_ROUTES } from './app.routes';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
